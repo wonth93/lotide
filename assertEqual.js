0ee5ab70e4624e916197ecb0f372d531a055864f
wonth93 <wongth93@gmail.com>
2022-08-14 00:06:49 +0000

    updated assertEqual.js to use template literals

--- assertEqual.js
+++ assertEqual.js
@@ -3,9 +3,9 @@ const wrongEmoji = String.fromCodePoint(0x274C);
 
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(correctEmoji + correctEmoji + correctEmoji + " Assertion Passed: " + actual + " === " + expected);
+    console.log(`${correctEmoji}${correctEmoji}${correctEmoji} Assertion Passed: ${actual} === ${expected}`);
   } else if (actual !== expected) {
-    console.log(wrongEmoji + wrongEmoji + wrongEmoji + " Assertion Failed: ", actual + " !== " + expected);
+    console.log(`${wrongEmoji}${wrongEmoji}${wrongEmoji} Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
