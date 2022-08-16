7120a3d5bea3664a46ed6fa4488095ea4dce6aad
wonth93 <wongth93@gmail.com>
2022-08-16 06:41:19 +0000

    updated assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,17 +1,25 @@
 const correctEmoji = String.fromCodePoint(0x2705);
 const wrongEmoji = String.fromCodePoint(0x274C);
 
-const assertAssaysEqual = function(arrayA, arrayB) {
+const eqArrays = function(arrayA, arrayB) {
   if (arrayA.length !== arrayB.length) {
-    return console.log(`${wrongEmoji}${wrongEmoji}${wrongEmoji} Assertion Failed: arrayA !== arrayB`);
+    return false;
   }
   for (let i = 0; i < arrayA.length; i++) {
     if (arrayA[i] !== arrayB[i]) {
-      return console.log(`${wrongEmoji}${wrongEmoji}${wrongEmoji} Assertion Failed: arrayA !== arrayB`);
+      return false;
     }
   }
-  return console.log(`${correctEmoji}${correctEmoji}${correctEmoji} Assertion Passed: arrayA === arrayB`);
+  return true;
 };
+
+const assertAssaysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`${correctEmoji}${correctEmoji}${correctEmoji} Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`${wrongEmoji}${wrongEmoji}${wrongEmoji} Assertion Failed: ${array1} !== ${array2}`);
+  }
+}
 
 
 // testing code
@@ -20,4 +28,4 @@ assertAssaysEqual([1, 2, 3], [3, 2, 1]);
 assertAssaysEqual(["1", "2", "3"], ["1", "2", "3"]);
 assertAssaysEqual([1, 2, 3], [1, 2, "3"]);
 assertAssaysEqual([1, 2, 3], [1, 2, 3, 4]);
-assertAssaysEqual([1, 1, 3], [1, true, 3]);
+assertAssaysEqual([1, 1, 0], [1, true, false]);
