f5a5672c093064da592af0623e6cbef8fb814f74
wonth93 <wongth93@gmail.com>
2022-08-16 22:53:42 +0000

    typo correction

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -13,19 +13,19 @@ const eqArrays = function(arrayA, arrayB) {
   return true;
 };
 
-const assertAssaysEqual = function(array1, array2) {
+const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
     console.log(`${correctEmoji}${correctEmoji}${correctEmoji} Assertion Passed: ${array1} === ${array2}`);
   } else {
     console.log(`${wrongEmoji}${wrongEmoji}${wrongEmoji} Assertion Failed: ${array1} !== ${array2}`);
   }
-}
+};
 
 
 // testing code
-assertAssaysEqual([1, 2, 3], [1, 2, 3]);
-assertAssaysEqual([1, 2, 3], [3, 2, 1]);
-assertAssaysEqual(["1", "2", "3"], ["1", "2", "3"]);
-assertAssaysEqual([1, 2, 3], [1, 2, "3"]);
-assertAssaysEqual([1, 2, 3], [1, 2, 3, 4]);
-assertAssaysEqual([1, 1, 0], [1, true, false]);
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual([1, 2, 3], [1, 2, "3"]);
+assertArraysEqual([1, 2, 3], [1, 2, 3, 4]);
+assertArraysEqual([1, 1, 0], [1, true, false]);
