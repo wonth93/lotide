793cc2d569da3c5ed6ca46619dc2d914a352a421
wonth93 <wongth93@gmail.com>
2022-08-20 01:52:15 +0000

    added eqObjects.js

---
+++ eqObjects.js
@@ -0,0 +1,58 @@
+const correctEmoji = String.fromCodePoint(0x2705);
+const wrongEmoji = String.fromCodePoint(0x274C);
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`${correctEmoji}${correctEmoji}${correctEmoji} Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`${wrongEmoji}${wrongEmoji}${wrongEmoji} Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const eqArrays = function(arrayA, arrayB) {
+  if (arrayA.length !== arrayB.length) {
+    return false;
+  }
+  for (let i = 0; i < arrayA.length; i++) {
+    if (arrayA[i] !== arrayB[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length === Object.keys(object2).length) {
+    for (const key of Object.keys(object1)) {
+      if (Array.isArray(object1[key]) === true && Array.isArray(object2[key]) === true) {
+        eqArrays(object1[key], object2[key]);
+      } else {
+        if (object1[key] !== object2[key]) {
+          return false;
+        }        
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+};
+
+
+// conde testing area
+const ab = { a: "1", b: "2" };
+const ba = { a: "1", b: "2" };
+const cc = { a: "1", b: "1" };
+// assertEqual(eqObjects(ab, ba), true);
+// assertEqual(eqObjects(ab, cc), false);
+const abc = { a: "1", b: "2", c: "3" };
+// assertEqual(eqObjects(ab, abc), false);
+
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+// assertEqual(eqObjects(cd, dc), true);
+
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+// assertEqual(eqObjects(cd, cd2), false);
+console.log(eqObjects(cd, cd2))
