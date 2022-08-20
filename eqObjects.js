00ef621dde59f79ce0a755db7c9d81a6f5a430a5
wonth93 <wongth93@gmail.com>
2022-08-20 02:27:16 +0000

    update coding

--- eqObjects.js
+++ eqObjects.js
@@ -26,11 +26,11 @@ const eqObjects = function(object1, object2) {
   }
   for (const key of Object.keys(object1)) {
     if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-      eqArrays(object1[key], object2[key]);
+      return eqArrays(object1[key], object2[key]);
     } else {
       if (object1[key] !== object2[key]) {
         return false;
-      }        
+      }
     }
   }
   return true;
@@ -38,20 +38,20 @@ const eqObjects = function(object1, object2) {
 
 
 // conde testing area
-const ab = { a: "1", b: "2" };
-const ba = { a: "1", b: "2" };
-const cc = { a: "1", b: "1" };
+// const ab = { a: "1", b: "2" };
+// const ba = { a: "1", b: "2" };
+// const cc = { a: "1", b: "1" };
 // assertEqual(eqObjects(ab, ba), true);
 // assertEqual(eqObjects(ab, cc), false);
-const abc = { a: "1", b: "2", c: "3" };
+// const abc = { a: "1", b: "2", c: "3" };
 // assertEqual(eqObjects(ab, abc), false);
 
 
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
+// const cd = { c: "1", d: ["2", 3] };
+// const dc = { d: ["2", 3], c: "1" };
 // assertEqual(eqObjects(cd, dc), true);
 
 
-const cd2 = { c: "1", d: ["2", 3, 4] };
+// const cd2 = { c: "1", d: ["2", 3, 4] };
 // assertEqual(eqObjects(cd, cd2), false);
 // console.log(eqObjects(cd, cd2))
