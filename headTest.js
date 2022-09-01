c686253c7c6c2544113b155811430252e22561f5
wonth93 <70833838+wonth93@users.noreply.github.com>
2022-08-31 23:28:24 -0700

    Delete headTest.js

--- headTest.js
+++
@@ -1,8 +0,0 @@
-const head = require('../head');
-const assertEqual = require('../assertEqual');
-
-// Test Code Area
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([555]), 555);
-assertEqual(head([]), undefined);
