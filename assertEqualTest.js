e125ddeee2301f0d329f586c402e00c26af404a0
wonth93 <wongth93@gmail.com>
2022-09-01 06:11:58 +0000

    upload test

---
+++ assertEqualTest.js
@@ -0,0 +1,6 @@
+const assertEqual = require('../assertEqual');
+
+// Test Code Area
+assertEqual("a", "a") // return Assertion Passed
+assertEqual("1", 1) // return Assertion Failed
+assertEqual(1, 1) // return Assertion Passed
