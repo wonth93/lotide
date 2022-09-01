2199c8ef86b007327133ad5842117b10735c2462
wonth93 <70833838+wonth93@users.noreply.github.com>
2022-08-31 23:28:32 -0700

    Delete assertEqualTest.js

--- assertEqualTest.js
+++
@@ -1,6 +0,0 @@
-const assertEqual = require('../assertEqual');
-
-// Test Code Area
-assertEqual("a", "a") // return Assertion Passed
-assertEqual("1", 1) // return Assertion Failed
-assertEqual(1, 1) // return Assertion Passed
