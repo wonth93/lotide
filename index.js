7ef533f44be38885925acf032da4d10e50ba0182
wonth93 <wongth93@gmail.com>
2022-09-02 06:11:33 +0000

    upload index.js

---
+++ index.js
@@ -0,0 +1,37 @@
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const without = require('./without');
+const takeUntil = require('./takeUntil');
+const map = require('./map');
+const flatten = require('./flatten');
+const findKeyByValue = require('./findKeyByValue');
+const findKey = require('./findKey');
+const eqObjects = require('./eqObjects');
+const eqArrays = require('./eqArrays');
+const countOnly = require('./countOnly');
+const countLetters = require('./countLetters');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const assertEqual = require('./assertEqual');
+const assertArraysEqual = require('./assertArraysEqual');
+const letterPositions = require('./letterPositions');
+
+module.exports = {
+  head:   head,
+  tail:   tail,
+  middle: middle,
+  without: without,
+  takeUntil: takeUntil,
+  map: map,
+  flatten: flatten,
+  findKeyByValue: findKeyByValue,
+  findKey: findKey,
+  eqObjects: eqObjects,
+  eqArrays: eqArrays,
+  countOnly: countOnly,
+  countLetters: countLetters,
+  assertObjectsEqual: assertObjectsEqual,
+  assertEqual: assertEqual,
+  assertArraysEqual: assertArraysEqual,
+  letterPositions: letterPositions
+};
