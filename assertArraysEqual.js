98fef86fabe20c1f505b4ef84cc9d8d660a11fbf
wonth93 <wongth93@gmail.com>
2022-09-02 01:13:03 +0000

    refactor assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -3,19 +3,6 @@ const eqArrays = require('./eqArrays');
 const correctEmoji = String.fromCodePoint(0x2705);
 const wrongEmoji = String.fromCodePoint(0x274C);
 
-// const eqArrays = function(arrayA, arrayB) {
-//   if (arrayA.length !== arrayB.length) {
-//     return false;
-//   }
-//   for (let i = 0; i < arrayA.length; i++) {
-//     if (arrayA[i] !== arrayB[i]) {
-//       return false;
-//     }
-//   }
-//   return true;
-// };
-
-
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
     console.log(`${correctEmoji}${correctEmoji}${correctEmoji} Assertion Passed: ${array1} === ${array2}`);
@@ -23,14 +10,5 @@ const assertArraysEqual = function(array1, array2) {
     console.log(`${wrongEmoji}${wrongEmoji}${wrongEmoji} Assertion Failed: ${array1} !== ${array2}`);
   }
 };
-
-
-// testing code
-// assertArraysEqual([1, 2, 3], [1, 2, 3]);
-// assertArraysEqual([1, 2, 3], [3, 2, 1]);
-// assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
-// assertArraysEqual([1, 2, 3], [1, 2, "3"]);
-// assertArraysEqual([1, 2, 3], [1, 2, 3, 4]);
-// assertArraysEqual([1, 1, 0], [1, true, false]);
 
 module.exports = assertArraysEqual;
